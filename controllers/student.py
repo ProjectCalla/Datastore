d5c56623c9a62196e83facc60f361874af5d93c0
Geddy05 <geddyschellevis@gmail.com>
2015-05-19 14:22:53 +0200

    added Check class to student controller

--- controllers/student.py
+++ controllers/student.py
@@ -7,3 +7,8 @@ class test(webapp2.RequestHandler):
     def post(self):
         a = "testen"
         logging.info(a);
+
+
+class Check(object):
+    def check_student(self,studentnr,password):
+        logging.info("We here")
