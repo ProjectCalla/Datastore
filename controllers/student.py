221b80514cfda8b307ebbf97a802961598db48f3
Geddy05 <geddyschellevis@gmail.com>
2015-05-19 13:59:45 +0200

    Add taskqueue sample to controllers

--- controllers/student.py
+++ controllers/student.py
@@ -1,1 +1,9 @@
-__author__ = 'geddyS'
+
+from google.appengine.api import taskqueue
+import webapp2
+import logging
+
+class test(webapp2.RequestHandler):
+    def post(self):
+        a = "testen"
+        logging.info(a);
